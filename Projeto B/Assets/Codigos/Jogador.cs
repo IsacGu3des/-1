594dc77f4e51f2fefefa44cc1e419d144c3db638
IsacGu3des <isac.pinheiro@escolar.ifrn.edu.br>
2023-09-28 16:09:42 -0200

    Codigo pulo

--- Projeto B/Assets/Codigos/Jogador.cs
+++ Projeto B/Assets/Codigos/Jogador.cs
@@ -7,18 +7,29 @@ public class Jogador: MonoBehaviour
 {
     private int velocidade = 10;
     private Rigidbody rb;
+    public int forcaPulo = 7;
+    public bool noChao;
+
 
     // Start is called before the first frame update
     void Start()
     {
         TryGetComponent(out rb);
     }
+    private void OnCollisionEnter(Collision collision){
+        throw new NotImplementedExcepition();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         float H = Input.GetAxis("Horizontal");
         float V = Input.GetAxis("Vertical");
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+        }
+        
         
         Vector3 direcao = new Vector3(H,0,V);
         rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
